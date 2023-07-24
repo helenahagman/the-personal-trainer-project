53b9c3abe246dfc40b63ca079e8fdea80472bc40
helenahagman <>
2023-07-24 10:06:00 +0000

    installed django-allauth

--- run.py
+++ run.py
@@ -15,17 +15,22 @@ def index():
 @app.route("/pt")
 def pt():
     active_page = 'trainer'
-    return render_template("pt.html", page_title="Personal Trainer", active_page=active_page)
+    return render_template("pt.html", page_title="Personal Trainer",
+                           active_page=active_page)
+
 
 @app.route("/member")
 def member():
     active_page = 'member'
-    return render_template("member.html", page_title="Members", active_page=active_page)
+    return render_template("member.html", page_title="Members",
+                           active_page=active_page)
+
 
 @app.route("/pt/book")
 def book():
     active_page = 'book'
-    return render_template("book.html", page_title="Book")
+    return render_template("book.html", page_title="Book",
+                           active_page=active_page)
 
 
 if __name__ == "__main__":
