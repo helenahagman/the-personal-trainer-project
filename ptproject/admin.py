7dddcac0a0cb5c07c43fa76d1610b77965ea5f03
helenahagman <>
2023-07-26 11:38:44 +0000

    added def to views.py file for booking session

--- ptproject/admin.py
+++ ptproject/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import BookingSession
 
 
-
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = BookingSession
+        fields = ['date', 'time']
--- ptproject/views.py
+++ ptproject/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BookingForm
 
-# Create your views here.
+
+def book_session request:
+    if request.method == 'POST'
+    form = BookingForm('request.POST')
+    if form.is_valid():
+        form.save()
+        # Redirect to success page
+        return redirect('success_page')
+    else:
+        form = BookingForm()
+    return render(request, 'booking_form.html', {'form': form})
