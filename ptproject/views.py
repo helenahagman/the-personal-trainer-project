059530046b43385de8ba2d879192a1c5f8bdab87
helenahagman <>
2023-07-27 16:02:48 +0000

    updated base.html to be more compatibla with django. added classes in views.py

--- ptproject/views.py
+++ ptproject/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render, redirect
-from django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic, View
 from .models import BookingForm
 
 
+class Home(generic.TemplateView):
+    #Opens start page
+    template_name = "index.html"
+
+
+class pt(generic.TemplateView):
+    # Opens personal trainer page
+    template_name = "pt.html"
+
+
+class Member(generic.TemplateView):
+    # Opens member page
+    template_name = "member.html"
+
+
 class BookingSession(models.Model):
+    model = BookingForm
     template_name = 'book.html'
     date = models.DateField()
     time = models.TimeField()
