be5155906c471bfa17cf359c4bd38002af61490c
helenahagman <>
2023-07-28 06:31:05 +0000

    updated urlpattern for index page

--- ptproject/urls.py
+++ ptproject/urls.py
@@ -3,7 +3,7 @@ from ptproject.views import Home, pt, Member, book_session
 from . import views
 
 urlpatterns = [
-    path("", views.Home.as_view(), name="home"),
+    path("", views.Index.as_view(), name="index"),
     path("pt/", views.pt.as_view(), name="pt"),
     path("member/", views.Member.as_view(), name="member"),
     path("booking/", views.book_session, name="book_session"),
